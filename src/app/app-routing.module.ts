ed6381859c2d0c8746ba116e8ca79c7643b9f811
raymondngiam <ha.ngiam87@gmail.com>
2023-01-17 22:49:20 +0800

    feat: add feat component with layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from 'src/app/product/product-list/product-list.component';
 import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+import { CartComponent } from './cart/cart/cart.component';
 
 const routes: Routes = [
   { path: '', component: ProductListComponent },
-  { path: 'item-detail/:id', component: ProductDetailComponent }
+  { path: 'item-detail/:id', component: ProductDetailComponent },
+  { path: 'cart', component: CartComponent }
 ];
 
 @NgModule({
