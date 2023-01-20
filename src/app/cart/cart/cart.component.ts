66026f8024edde1d7b2dc64ea4ad11acb8775ff3
raymondngiam <ha.ngiam87@gmail.com>
2023-01-21 00:33:56 +0800

    feat: add calculate total price in cart

--- src/app/cart/cart/cart.component.ts
+++ src/app/cart/cart/cart.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
 import { CartItem } from 'src/app/models/cart-item';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-cart',
@@ -9,20 +11,40 @@ import { CartItem } from 'src/app/models/cart-item';
 })
 export class CartComponent implements OnInit {
   cartItems: CartItem[] = [];
+  products: Product[] = [];
+  totalPrice = 0;
 
-  constructor(private cartService: CartService) {}
+  constructor(
+    private cartService: CartService,
+    private productService: ProductService
+  ) {}
 
   ngOnInit(): void {
     this.cartItems = this.cartService.getCartItems();
+    this.productService.getProductList().subscribe((t) => {
+      this.products = t;
+      this.updateTotalPrice();
+    });
+  }
+
+  updateTotalPrice(): void {
+    this.totalPrice = 0;
+    for (const item of this.cartItems) {
+      const price = this.products.filter((p) => p.id == item.productId)[0]
+        .price;
+      this.totalPrice += price * item.quantity;
+    }
   }
 
   onUpdateQty() {
     this.cartService.updateCartItems(this.cartItems);
     alert(`Quantity updated.`);
+    this.updateTotalPrice();
   }
 
   deleteCartItem(productId: number) {
     this.cartService.deleteCartItem(productId);
     this.cartItems = this.cartService.getCartItems();
+    this.updateTotalPrice();
   }
 }
